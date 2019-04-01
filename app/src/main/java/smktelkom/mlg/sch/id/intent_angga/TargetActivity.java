63e203e45ff6c63986973f50d3432049a60e8d44
Pramuja <mohamad_angga_26rpl@student.smktelkom-mlg.sch.id>
2019-04-01 18:31:59 +0700

    Selesai

---
+++ app/src/main/java/smktelkom/mlg/sch/id/intent_angga/TargetActivity.java
@@ -0,0 +1,23 @@
+package smktelkom.mlg.sch.id.intent_angga;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TargetActivity extends AppCompatActivity {
+
+    private TextView txtResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_target);
+        txtResult = (TextView) findViewById(R.id.textView);
+
+        //getData
+        if (getIntent().getExtras() != null){
+            String data = getIntent().getExtras().getString(MainActivity.EXTRA_DATA);
+            txtResult.setText(data);
+        }
+    }
+}
